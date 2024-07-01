27205d36bd366e7d6fdf8f275dd9fb3107c405f2
maegon <dlagorhs99@naver.com>
2024-07-01 16:31:25 +0900

    routing

---
+++ frontapp/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import '../app.css';
+</script>
+
+<div class="text-orange-600">레이아웃</div>
+<a href="/articles">게시글 목록</a>
+<a href="/test">테스트</a>
+<slot />
--- frontapp/src/routes/layout.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import '../app.css';
-</script>
-
-<div class="text-orange-600">레이아웃</div>
-<slot />
